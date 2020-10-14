72e9cf5aed42f7a8ba5dc662b078bcc94e56e222
sha <sha@systemate.dk>
2020-10-14 13:50:17 +0200

    Add CircularStringBuffer by source tree copy

---
+++ src/Generics/CircularStringBuffer.cs
@@ -0,0 +1,52 @@
+﻿namespace Generics
+{
+    public class CircularStringBuffer
+    {
+        private string[] _buffer;
+        private int _start;
+        private int _end;
+
+        public CircularStringBuffer () : this(capacity:10)
+        {
+        }
+        
+        public CircularStringBuffer(int capacity)
+        {
+            _buffer = new string[capacity+1];
+            _start = 0;
+            _end = 0;
+        }
+
+        public void Write(string value)
+        {
+            _buffer[_end] = value;
+            _end = (_end + 1) % _buffer.Length;
+            if (_end == _start)
+            {
+                _start = (_start + 1) % _buffer.Length;
+            }
+        }
+
+        public string Read()
+        {
+            var result = _buffer[_start];
+            _start = (_start + 1) % _buffer.Length;
+            return result;
+        }
+
+        public int Capacity 
+        { 
+            get { return _buffer.Length; } 
+        }
+
+        public bool IsEmpty 
+        {
+            get { return _end == _start; }
+        }
+
+        public bool IsFull 
+        {
+            get { return (_end + 1) % _buffer.Length == _start;  }
+        }   
+    }
+}
