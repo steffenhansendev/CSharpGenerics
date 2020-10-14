def63d1667ee7db2ab12f4c50204ca040a64ecdc
sha <sha@systemate.dk>
2020-10-14 14:59:14 +0200

    Add collection exercise files

---
+++ src/GenericCollections/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace GenericCollections
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
---
+++ src/GenericCollections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[2]
+            {
+                new Employee {Name = "Scott"},
+                new Employee {Name = "Alex"}
+            };
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+
+            Array.Resize(ref employees, 3);
+
+            employees[2] = new Employee {Name = "Brian"};
+        }
+    }
+}
