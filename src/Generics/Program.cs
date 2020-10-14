c94369b236f141b4d997f24d1162d1d462f7a82a
sha <sha@systemate.dk>
2020-10-14 11:22:20 +0200

    Add exercise files

--- src/Generics/Program.cs
+++ src/Generics/Program.cs
@@ -6,7 +6,30 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var buffer = new CircularBuffer(capacity: 3);
+
+            while(true)
+            {
+                var value = 0.0;
+                var input = Console.ReadLine();
+
+                if (double.TryParse(input, out value))
+                {
+                    buffer.Write(value);
+                    continue;
+                }
+                break;
+            }
+
+            var sum = 0.0;
+            Console.WriteLine("Buffer: ");
+            while(!buffer.IsEmpty)
+            {
+                double value = buffer.Read();
+                Console.WriteLine(value);
+                sum += value;
+            }
+            Console.WriteLine("Sum: " + sum);
         }
     }
 }
---
+++ test/Generics.Tests/CircularBufferTests.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+
+namespace Generics.Tests
+{
+    public class CircularBufferTests
+    {
+        [Fact]
+        public void New_Buffer_Is_Empty()
+        {
+            var buffer = new CircularBuffer();
+            Assert.True(buffer.IsEmpty);
+        }
+
+        [Fact]
+        public void Three_Element_Buffer_Is_Full_After_Three_Writes()
+        {
+            var buffer = new CircularBuffer(capacity: 3);
+            buffer.Write(1);
+            buffer.Write(1);
+            buffer.Write(1);
+            Assert.True(buffer.IsFull);
+        }
+
+        [Fact]
+        public void First_In_First_Out_When_Not_Full()
+        {
+            var buffer = new CircularBuffer(capacity: 3);
+            var value1 = 1.1;
+            var value2 = 2.0;
+
+            buffer.Write(value1);
+            buffer.Write(value2);
+
+            Assert.Equal(value1, buffer.Read());
+            Assert.Equal(value2, buffer.Read());
+            Assert.True(buffer.IsEmpty);
+        }
+
+        [Fact]
+        public void Overwrites_When_More_Than_Capacity()
+        {
+            var buffer = new CircularBuffer(capacity: 3);
+            var values = new[] { 1.0, 2.0, 3.0, 4.0, 5.0 };
+
+            foreach (var value in values)
+            {
+                buffer.Write(value);
+            }
+
+            Assert.True(buffer.IsFull);
+            Assert.Equal(values[2], buffer.Read());
+            Assert.Equal(values[3], buffer.Read());
+            Assert.Equal(values[4], buffer.Read());
+            Assert.True(buffer.IsEmpty);
+        }
+    }
+}
